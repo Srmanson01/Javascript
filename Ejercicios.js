83402627d83ea4d9c7ce03edcaafffbdca004c2f
Srmanson01 <renoxarmin10@gmail.com>
2022-01-05 17:50:27 -0500

    charged

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,11 +1,7 @@
 /*function miprimera() {
 console.log ("arroz")
 
-console.log("leche")
-return 20;
-console.log("azucar")
 
-return "retorna lo que yo quiero";
     
 }
 
@@ -13,49 +9,24 @@ let impfunction = miprimera();
 
 
 
-console.log (impfunction)
-
-/*const mifuncion = function () {
-    console.log("Mi funcion anonima ya declarada")
-    
-}
-
-mifuncion(); */
-
-/*try {
-    console.log("En el try se agrega el codigo a evaluar")
-} catch (error) {
-    console.log("Capturamos el error surgido en el try")
-} finally{
-    console.log("Este bloque se ejecutara siempre al final try-catch")
-}*/
-
-/*const num1 = [1,2,3,4,5],
-        num2 = [6,7,8,9,0]
-
-        console.log(num1,num2)
- 
-const num3 = [...num1,...num2]
-console.log(num3)
-
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.push("Kiwi");*/
 
 
 
-/*const fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-fruits[fruits.length] = "Kiwi";
-fruits[fruits.length] = "pera";
-
- delete fruits
- 
-
-console.log(fruits);*/
 
 
 
-const colores = ['blanco','negro','azul','rojo'];
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Sin arrow function
 
